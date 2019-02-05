6ac67fb914046e9180b2726b01dfbd56135fa8b8
Abbe-Engers <abbelaptop@gmail.com>
2019-02-05 15:43:54 +0100

    Update Code.js

---
+++ Code.js
@@ -0,0 +1,17 @@
+var EtenResultaat;
+var AmusementResultaat;
+var HotelResultaat;
+
+SaveResultaatEten ( EtenValue ) {
+  EtenResultaat = EtenValue;
+}
+
+
+SaveResultaatAmusement ( AmusementValue ) {
+  AmusementResultaat = AmusementValue;
+}
+
+
+SaveResultaatEten ( HotelValue ) {
+  HotelResultaat = HotelValue;
+}
